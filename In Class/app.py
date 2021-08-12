7db88994e497245a61809ead716551b968ec1f4c
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-08-12 20:33:15 +0300

    streamlit codes

---
+++ In Class/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# text/file
+st.title("Streamlit Tutorial")
+st.text("Hello streamlit")
+
+# header/subheader
+st.header("This is a header")
+st.subheader("This is a subheader")
+
+# markdown
+st.markdown("This is a markdown")
+st.markdown("**This is a markdown**")
+
+# colorful text
+st.succes("Successfull")
+st.info("This is Information")
+
+# help
+st.warning("This is a warning")
+st.error("Stop. That give an error")

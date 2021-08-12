a1b55529c20691b1f100c82905870a53e51300d5
Ertugrul-Kurubal <antikakurubal@gmail.com>
2021-08-12 22:38:45 +0300

    Streamlit In Class

--- In Class/app.py
+++ In Class/app.py
@@ -68,3 +68,44 @@ if st.button("About Program"):
     st.text("Streamlit is easy and fun")
 else:
     st.text("Nothing to say")
+    
+# text input
+firstname = st.text_input("Enter your firstname:")
+
+if st.button("Submit"):
+    st.success(firstname.title())
+    
+# text_area
+message = st.text_area("Enter your message", "type right here..")
+if st.button("submit"):
+    st.info(message.title())
+    
+# date input
+import datetime
+today = st.date_input("Today is", datetime.datetime.now())
+
+# time input
+my_time = st.time_input("The time is", datetime.time(22,15))
+
+# raw data
+st.text("display text")
+st.code("Import pandas as pd")
+
+# multiple line code
+with st.echo():
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    
+# sidebar
+st.sidebar.title("This is a sidebar")
+
+# read a dataframe
+import pandas as pd
+df = pd.read_csv("Mall_Customers.csv")
+                 
+st.table(df.head())
+
+st.write(df.head())
+
+st.dataframe(df.head())

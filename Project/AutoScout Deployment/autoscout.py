13e200cf2df2be2d476e1c4450a98cad55add138
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-08-15 16:58:26 +0300

    Features Completed

--- Project/AutoScout Deployment/autoscout.py
+++ Project/AutoScout Deployment/autoscout.py
@@ -4,11 +4,17 @@ import pandas as pd
 
 # Title
 st.header("AutoScout Car Price")
-
 st.subheader("Select Car Features And Predict Price")
+#from PIL import Image
+#img = Image.open("Machine Learning.jpg")
+#st.image(img, width=200, caption="my_image")
+
 
 
 
 st.sidebar.success("Features Selection")
-st.sidebar.radio('Select Car Model', ["Audi","BMW","Mercedes"])
+st.sidebar.radio('Select Car Model', ['A1','A3','Astra','Corsa','Insignia','Clio','Espace'])
 st.sidebar.radio('Select Gearing Type', ["Automatic","Semi-Auto","Manuel"])
+st.sidebar.slider("Power Of The Car (hp)",1,1000)
+st.sidebar.slider("Mileage Of The Car (km)",0,2000000)
+st.sidebar.slider("Age Of The Car (year)",0,100)

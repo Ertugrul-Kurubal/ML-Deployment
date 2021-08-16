472ec630b1770232ab12d46755edf3315791c58f
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-08-16 19:25:36 +0300

    Project Solution

--- Project/AutoScout Deployment/autoscout.py
+++ Project/AutoScout Deployment/autoscout.py
@@ -1,29 +1,57 @@
 import streamlit as st
+from sklearn.preprocessing import StandardScaler
+from PIL import Image
 import pickle
 import pandas as pd
 
 # Title
 st.header("AutoScout Car Price")
 st.subheader("Select Car Features And Predict Price")
-#from PIL import Image
-#img = Image.open("Machine Learning.jpg")
-#st.image(img, width=200, caption="my_image")
 
 
-
-
-st.sidebar.success("Features Selection")
+st.sidebar.header("Features Selection")
 model = st.sidebar.radio('Select Car Model', ['A1','A3','Astra','Corsa','Insignia','Clio','Espace'])
 gearing_type = st.sidebar.radio('Select Gearing Type', ["Automatic","Semi-Auto","Manuel"])
 hp = st.sidebar.slider("Power Of The Car (hp)",1,1000)
 km = st.sidebar.slider("Mileage Of The Car (km)",0,2000000)
 age = st.sidebar.slider("Age Of The Car (year)",0,100)
 
-# Model
+if model == "A1":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\A1_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "A3":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\A3_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "Astra":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\Astra_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "Clio":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\Clio_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "Corsa":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\Corsa_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "Espace":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\Espace_1.jpg")
+    st.image(img, caption=f"{model}")
+elif model == "Insignia":
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\Insignia_1.jpg")
+    st.image(img, caption=f"{model}")
+else:
+    img = Image.open("D:\Data Scientist\Data Science\ML-Deployment\Project\AutoScout Deployment\All.jpg")
+    st.image(img, caption=f"{model}")
+
+
+
+
+# Model Load
 model=pickle.load(open("lasso_final_model", "rb"))
 
-# Scale
+# Scale Load
 scaler=pickle.load(open("scaler", "rb"))
+
+# Columns Load
+X_columns = pickle.load(open("colums", "rb"))
 
 # Data
 my_dict = {
@@ -36,13 +64,17 @@ my_dict = {
 
 # DataDrame
 my_dict = pd.DataFrame([my_dict])
-my_dict
 
-my_dict = pd.get_dummies(my_dict).reindex(columns=X.columns, fill_value=0)
-my_dict
+# Dummies
+my_dict = pd.get_dummies(my_dict).reindex(columns=X_columns, fill_value=0)
 
 # Scale
 my_dict = scaler.transform(my_dict)
 
 # Predict
-model.predict(my_dict)
+pred_result = model.predict(my_dict)
+
+# st.subheader("Press Predict")
+if st.button("Predict"):
+    prediction = pred_result
+    st.success("The Car Price Prediction is {}. ".format(int(prediction)))

0365997c5b8b3fea6c321233e4cdbb6b14839dc8
Ertuğrul Kurubal <antikakurubal@gmail.com>
2021-08-16 16:45:45 +0300

    Data

--- Project/AutoScout Deployment/autoscout.py
+++ Project/AutoScout Deployment/autoscout.py
@@ -21,3 +21,24 @@ age = st.sidebar.slider("Age Of The Car (year)",0,100)
 
 # Model
 model=pickle.load(open("lasso_final_model", "rb"))
+
+# Data
+my_dict = {
+    "hp": hp,
+    "age": age,
+    "km": km,
+    "model": model,
+    "gearing_type": gearing_type
+}
+
+# DataDrame
+my_dict = pd.DataFrame([my_dict])
+my_dict
+
+my_dict = pd.get_dummies(my_dict).reindex(columns=X.columns, fill_value=0)
+my_dict
+
+# Scale
+my_dict = scaler.transform(my_dict)
+
+model.predict(my_dict)
